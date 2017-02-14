696f3e01b574057a223be541271deb20a334f7de
pierre roze <pierre.roze@ieseg.fr>
2017-02-14 16:01:41 -0200

    Ex 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
+
+
+get'tasks', to: "tasks#index"
+
+get'task/:id', to: "tasks#show", as: "created_task"
+
+get 'tasks/new', to:"tasks#new"
+
+post 'tasks', to:"tasks#create"
+
+get 'tasks/:id/edit', to: "tasks#edit", as: "updated_task"
+
+patch 'tasks/:id', to: "tasks#update"
+
+delete 'tasks/:id', to: "tasks#delete", as:"deleted_task"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+
