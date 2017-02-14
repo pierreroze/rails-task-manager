d382425ecc1130d14044bbabcd71cd2e2d4c4ecb
pierre roze <pierre.roze@ieseg.fr>
2017-02-14 11:08:36 -0200

    added class model

---
+++ db/migrate/20170214130645_create_tasks.rb
@@ -0,0 +1,10 @@
+class CreateTasks < ActiveRecord::Migration[5.0]
+  def change
+    create_table :tasks do |t|
+      t.boolean :done
+      t.string :name
+
+      t.timestamps
+    end
+  end
+end
